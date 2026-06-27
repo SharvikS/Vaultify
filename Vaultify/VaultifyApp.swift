637c1b08435bb4c27414ef942c211ee0b0193d53
Sharvik Sutar <sharviksutar@gmail.com>
2026-06-27 18:04:58 +0530

    Initial Commit

---
+++ Vaultify/VaultifyApp.swift
@@ -0,0 +1,32 @@
+//
+//  VaultifyApp.swift
+//  Vaultify
+//
+//  Created by Sharvik Sutar on 27/06/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct VaultifyApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
